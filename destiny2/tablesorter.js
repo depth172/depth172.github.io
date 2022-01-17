4fb900efa83067692b8e3d045f73c3fe267a0921
depth172 <depth172@yahoo.co.jp>
2022-01-17 18:27:45 +0900

    バレル・マガジンのページを仮設置

---
+++ destiny2/tablesorter.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+    $('#barrel_table').tablesorter({
+        headers: {
+            0: { sorter: false },
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('#barrel_table_sg').tablesorter({
+        headers: {
+            0: { sorter: false },
+            5: { sorter: false },
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('#magazine_table1').tablesorter({
+        headers: {
+            0: { sorter: false },
+            6: { sorter: false },
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('#magazine_table2').tablesorter({
+        headers: {
+            0: { sorter: false },
+            7: { sorter: false },
+        }
+    });
+});
+
+$(document).ready(function() {
+    $('#magazine_table_sg').tablesorter({
+        headers: {
+            0: { sorter: false },
+        }
+    });
+});
